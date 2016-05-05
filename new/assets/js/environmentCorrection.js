acf19c3f8e63df1740cbb52e5477543de894f4ce
abhinavagrawal1995 <abhinavagrawal1995@gmail.com>
2016-05-05 07:55:11 +0530

    task 3 done

--- new/assets/js/environmentCorrection.js
+++
@@ -1,8 +0,0 @@
-
-rangy.rangePrototype.insertNodeAtEnd = function (node) {
-	var range = this.cloneRange();
-	range.collapse(false);
-	range.insertNode(node);
-	range.detach();
-	this.setEndAfter(node);
-};
